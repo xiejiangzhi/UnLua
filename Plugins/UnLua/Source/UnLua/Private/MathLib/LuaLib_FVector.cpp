4a45fe6e046fc9d207764765c9670c80356dd16a
xiejiangzhi <xiejiangzhi@gmail.com>
2022-07-04 14:00:57 +0800

    Add FVector.GetSafeNormal and  FVector.GetUnsafeNormal

--- Plugins/UnLua/Source/UnLua/Private/MathLib/LuaLib_FVector.cpp
+++ Plugins/UnLua/Source/UnLua/Private/MathLib/LuaLib_FVector.cpp
@@ -1,15 +1,15 @@
 // Tencent is pleased to support the open source community by making UnLua available.
-// 
+//
 // Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
 //
-// Licensed under the MIT License (the "License"); 
+// Licensed under the MIT License (the "License");
 // you may not use this file except in compliance with the License. You may obtain a copy of the License at
 //
 // http://opensource.org/licenses/MIT
 //
-// Unless required by applicable law or agreed to in writing, 
-// software distributed under the License is distributed on an "AS IS" BASIS, 
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
 #include "UnLuaCompatibility.h"
@@ -141,6 +141,8 @@ static const luaL_Reg FVectorLib[] =
 BEGIN_EXPORT_REFLECTED_CLASS(FVector)
     ADD_CONST_FUNCTION_EX("Dot", unluaReal, operator|, const FVector&)
     ADD_CONST_FUNCTION_EX("Cross", FVector, operator^, const FVector&)
+    ADD_CONST_FUNCTION_EX("GetSafeNormal", FVector, GetSafeNormal, float)
+    ADD_CONST_FUNCTION_EX("GetUnsafeNormal", FVector, GetUnsafeNormal, float)
     ADD_FUNCTION(Size)
     ADD_FUNCTION(Size2D)
     ADD_FUNCTION(SizeSquared)
